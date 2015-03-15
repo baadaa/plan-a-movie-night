4f0a6d27ff03c257a22b4ae822fb972187234d58
Angel S. Moreno <angelxmoreno@gmail.com>
2015-03-15 08:46:49 -0400

    User shared instance; more models

--- 1_dev-files/Plan a Movie Night/Plan a Movie Night/MovieLineUp.swift
+++ 1_dev-files/Plan a Movie Night/Plan a Movie Night/MovieLineUp.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
-class MovieLineUp: BasePAMNModel {
-   
+class MovieLineUp: Serializable {
+    var event_id: String
+    var movie_id: String
+    var score: Float {
+        get {
+            return self.calculateScore()
+        }
+    }
+    
+    //@todo make these non optionals fetch in the init
+    private var event: Event?
+    private var movie: Movie?
+    
+    init(event_id: String, movie_id: String) {
+        self.event_id = event_id
+        self.movie_id = movie_id
+    }
+    
+    private func calculateScore() -> Float {
+        return Float(0);
+    }
+    
+    
 }
